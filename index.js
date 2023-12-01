b9364e6053805cd7c14ebcf111585ed029fbe9f9
venkateshreddy2003 <venkateshreddy2003@outlook.com>
2023-12-01 17:38:44 +0530

    Modified

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const username = process.env.username ?? "unu";
+const password = process.env.password ?? "unp";
+app.all("/", (req, res) => {
+  console.log("Just got a request!");
+  res.send(`Yo! ${username}   ${password}`);
+});
+app.listen(process.env.PORT || 3000);
