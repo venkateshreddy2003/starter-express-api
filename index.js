1eb0565e09984ec54fed90e855b034b86061b93f
venkateshreddy2003 <venkateshreddy2003@outlook.com>
2023-12-02 11:08:10 +0530

    modified

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-const username = process.env.username ?? "unu";
-const password = process.env.password ?? "unp";
+const config = process.env.secret;
 app.all("/", (req, res) => {
   console.log("Just got a request!");
-  res.send(process.env.key1 + " " + process.env.key2);
+  res.send(config?.key1 + " " + config?.key2);
 });
 app.listen(process.env.PORT || 3000);
