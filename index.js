83fc770c103693b94ff306400a55afb25c1cad20
venkateshreddy2003 <venkateshreddy2003@outlook.com>
2023-12-02 11:17:13 +0530

    Modified

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const config = process.env.secret;
 app.all("/", (req, res) => {
   console.log("Just got a request!");
+  console.log(config);
   res.send(config?.key1 + " " + config?.key2);
 });
 app.listen(process.env.PORT || 3000);
